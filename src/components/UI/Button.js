e8308518aa1e554d7de3380aef1209a79c56d576
Usama <usamasaeed1028@gmail.com>
2023-02-03 21:01:34 +0500

    updated code

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({children,className, onClick}) => {
   return (
-    <button className={`flex w-fit items-center border justify-center rounded-full font-bold border-transparent  ${className}`} onClick={onClick}>
+    <button className={`flex  items-center border justify-center rounded-full font-bold border-transparent  ${className}`} onClick={onClick}>
       {children}
     </button>
   );
