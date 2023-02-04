dc999ce5f9bce91bafa10c224e3578a0f15d4c63
Usama <usamasaeed1028@gmail.com>
2023-02-05 00:23:50 +0500

    updated code

--- src/components/UI/LocationModal.js
+++ src/components/UI/LocationModal.js
@@ -1,6 +1,6 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { Image } from "react-bootstrap";
 import Button from "./Button";
 
 const LocationModal = ({setLocationModal}) => {
